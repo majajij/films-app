32b5924ee060946fdb6fa5c3c86bb6afebda2050
MOURAD <mourad@MOURADs-MacBook-Pro.local>
2020-05-09 20:46:52 +0000

    add styles

--- src/components/bookmarks/bookmark/bookmark.js
+++ src/components/bookmarks/bookmark/bookmark.js
@@ -2,11 +2,7 @@ import React from "react";
 import classes from "./bookmark.module.scss";
 
 const BookMark = (props) => {
-  return (
-    <div>
-       BookMark
-    </div>
-  );
+  return <div className={classes.bookmark}>BookMark</div>;
 };
 
 export default BookMark;
